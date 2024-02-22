988055cf79c33c77c1806903a4b8b60288e99915
birgulkurtcu <birgulkurtcu@gmail.com>
2024-02-23 02:02:22 +0300

    bootcamproject-

---
+++ src/main/java/com/tobeto/bootcamproject/BootcamprojectApplication.java
@@ -0,0 +1,20 @@
+package com.tobeto.bootcamproject;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BootcamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcamprojectApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
+	}
+
+}
